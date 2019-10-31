c9857ba4d52f2cae29e99d4e8d9ec1e1e9fcfcd5
bcapuyan <36900689+bcapuyan@users.noreply.github.com>
2019-10-31 10:55:13 -0700

    Update login_example.php

--- login_example.php
+++ login_example.php
@@ -5,7 +5,7 @@ $str = "https://api.prismacloud.io/login";
 $ch = curl_init ($str);
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt ($ch, CURLOPT_POSTFIELDS, "{\"username\":\"0b7072c3-85cf-4bd7-9ae9-7b329c8cd6f9\",\"password\":\"5pQzsk0V10WqMieQl8Z27+ppO34=\",\"customerName\":\"boncap01\"}");
+curl_setopt ($ch, CURLOPT_POSTFIELDS, "{\"username\":\"[Access Key]\",\"password\":\"[Secret Key]\",\"customerName\":\"[Customer Name]\"}");
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
 $output = curl_exec ($ch);
